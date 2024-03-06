f42e17aaae20c8bc67bdb72a61da6ee171211a9a
GRI <gri-camp@yandex.ru>
2024-03-06 15:55:29 +0300

    add_readme

--- index.js
+++ index.js
@@ -2,29 +2,34 @@ import AutoSlider from "./gri-slider/index.js";
 
 const SLIDE_LIST = [
   {
-    slideImg:
-      "https://guitar.com/wp-content/uploads/2020/01/Jackson-charvel-double-neck@1400x1050.jpg",
-    comment: "love this one",
-    controlImg: "./src/icons/home.svg",
+    slideImg: "./picts/guitars/at_ibanez.jpg",
+    comment: "Andy Timmons <b>`CRYING`</b> Ibanez",
+    controlImg: "./src/icons/guitar.svg",
   },
   {
-    slideImg:
-      "http://www.hutchinsonguitars.com/wp-content/uploads/2014/11/IMG_2340.jpg",
+    slideImg: "./picts/guitars/ej_strat.jpg",
+    comment: "Eric Johnson's <b>`DESERT ROSE`</b> Fender Strat",
   },
   {
-    slideImg:
-      "https://i.pinimg.com/originals/c6/64/42/c664421ecd4b6a8551d7f338232f87c9.jpg",
-  },  
+    slideImg: "./picts/guitars/pg_FRM_150TR.jpg",
+    comment: "Paul Guilbert's <b>`SUPER HERO`</b> FRM_150TR Ibanez",
+  },
   {
-    slideImg:
-      "https://i.pinimg.com/originals/c6/64/42/c664421ecd4b6a8551d7f338232f87c9.jpg",
-    comment: "Such a vibe....",
+    slideImg: "./picts/guitars/sv_JEM777.jpg",
+    comment: "Steve Vai's <strong>` PRAYING `</strong> Ibanez JEM777",
+  },
+  {
+    slideImg: "./picts/guitars/ym_fender_strat.jpg",
+    comment: "Yngwee's <b>`FLASHING`</b> Fender Strat",
   },
 ];
+
+// ACCELERATE: ---
 new AutoSlider({
   list: SLIDE_LIST,
-  options: { fontFamily: "Roboto", color: "orangered" },
+  options: { fontFamily: "montserrat", color: "#a81843", letterSpacing: '0.7px' },
   isAutoSlider: true,
   panel: ["renderControls"],
   imgInSlideCount: 1,
+  delay: 2e3
 });
